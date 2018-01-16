ed2b0e51a1111e380b81fe90a12cfbbe17240fa9
ksdshpx <southeast_px@163.com>
2018-01-16 11:40:29 +0800

    POI操作Excel单元格格式!

--- src/main/java/cn/ksdshpx/poi/POIDemo.java
+++ src/main/java/cn/ksdshpx/poi/POIDemo.java
@@ -4,7 +4,10 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.extractor.ExcelExtractor;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.extractor.XSSFExcelExtractor;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
@@ -118,4 +121,46 @@ public class POIDemo {
         //excelExtractor.setIncludeSheetNames(false);
         System.out.println(excelExtractor.getText());
     }
+
+    @Test
+    public void test7() throws Exception{
+        //创建工作簿
+        Workbook workbook = new XSSFWorkbook();
+        //创建Sheet页
+        Sheet sheet = workbook.createSheet("第一个Sheet页");
+        //创建row
+        Row row = sheet.createRow(0);//创建第一行
+        row.setHeightInPoints(30);
+        Cell cell = createCell(workbook,row,(short)2, XSSFCellStyle.ALIGN_CENTER,XSSFCellStyle.VERTICAL_BOTTOM);
+        cell.setCellValue(new XSSFRichTextString("Excel好"));
+        OutputStream outputStream = new FileOutputStream("e:\\HelloExcel.xlsx");
+        workbook.write(outputStream);
+        outputStream.close();
+    }
+
+    /**
+     * 创建带有格式的单元格
+     * @param workbook
+     * @param row
+     * @param column
+     * @param halign
+     * @param valign
+     */
+    private Cell createCell(Workbook workbook,Row row,short column,short halign,short valign){
+        Cell cell = row.createCell(column);
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setAlignment(halign);
+        cellStyle.setVerticalAlignment(valign);
+        //设置边框格式
+        cellStyle.setBorderBottom(BorderStyle.DASHED);
+        cellStyle.setBottomBorderColor(IndexedColors.RED.getIndex());
+        //设置背景色、前景色
+        cellStyle.setFillBackgroundColor(IndexedColors.BLUE.getIndex());
+        cellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        //合并单元格
+        workbook.getSheetAt(0).addMergedRegion(new CellRangeAddress(2,3,1,2));
+        cell.setCellStyle(cellStyle);
+        return cell;
+    }
+
 }
